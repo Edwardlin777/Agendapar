5920216aa280841e9278d3e54b1d1c2ee6754563
unknowncoder05 <49960321+unknowncoder05@users.noreply.github.com>
2020-07-17 02:42:09 -0500

    limite porcentaje
    -ya en teoria no se puede pasar del 100%

--- yerson/pagina/add.php
+++ yerson/pagina/add.php
@@ -1,5 +1,15 @@
 <?php
+function get($conn,$sql)
+  {
+    $results = mysqli_query($conn, $sql);
+    $r = [];
 
+      while($row = mysqli_fetch_assoc($results)) {
+       array_push($r,$row);
+      }
+      return $r;
+
+  }
   $conn = mysqli_connect("localhost", "root", "", "agenda8");
   if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
@@ -12,6 +22,22 @@ echo $clase;
 switch ($clase) {
   case 'nota':
   {
+    $id_criterio=$_POST["id_criterio"];
+    $sql=
+    "
+    SELECT * FROM nota WHERE id_criterio like '$id_criterio' 
+    ";
+    $notas = get($conn,$sql);
+    $total=0;
+    for ($x = 0; $x < sizeof($notas); $x++)
+    {
+        $total+=$notas[$x]["peso_nota"];
+    }
+    if($total+$_POST["peso_nota"]>100)
+    {
+      echo "El porcentaje total es mayor que 100";
+      break;
+    }
 $sql ="INSERT INTO nota
     (
     id_nota,
@@ -33,6 +59,23 @@ break;
 
   case 'criterio':
   {
+    $id_corte=$_POST["id_corte"];
+    $sql=
+    "
+    SELECT * FROM criterio WHERE id_corte like '$id_corte' 
+    ";
+    $notas = get($conn,$sql);
+    $total=0;
+    for ($x = 0; $x < sizeof($notas); $x++)
+    {
+        $total+=$notas[$x]["peso_criterio"];
+    }
+    if($total+$_POST["peso_criterio"]>100)
+    {
+      echo "El porcentaje total es mayor que 100";
+      break;
+    }
+
 $sql ="INSERT INTO criterio
     (
     id_criterio,
